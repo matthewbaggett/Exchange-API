31d71ae298aa2d45e2164ab9d3e8ce2bacdd05cc
Matthew Baggett <matthew@baggett.me>
2014-03-21 10:08:49 +0000

    Change what we're guzzling.

--- src/ExchangeApi/Cryptsy/Valuations.php
+++ src/ExchangeApi/Cryptsy/Valuations.php
@@ -8,6 +8,7 @@ class Valuations{
     $client = new \Guzzle\Http\Client();
     $response = $client->get('http://pubapi.cryptsy.com/api.php?method=marketdatav2');
     $json = $response->getResponseBody();
+    var_dump($json);
     krumo(json_decode($json));exit;
   }
 }
