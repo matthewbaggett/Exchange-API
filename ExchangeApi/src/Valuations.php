ec1f8cb715364bf7fc5a94b3314e53628eef6022
Matthew Baggett <matthew@baggett.me>
2014-03-21 09:41:29 +0000

    Fix typo

--- ExchangeApi/src/Valuations.php
+++ ExchangeApi/src/Valuations.php
@@ -6,7 +6,7 @@ class Valuations{
 
   static public function fetch(){
     foreach(Exchanges::get_exchange_list() as $exchange){
-      $name = "\\ExchangeApi\\{$exchange}\\Valuation";
+      $name = "\\ExchangeApi\\{$exchange}\\Valuations";
       $valuation = $name::fetch();
       var_dump($valuation);
       exit;
